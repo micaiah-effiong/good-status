37d3c6133c75ea79d1fe3da47e47b73e51709adc
micaiah-effiong <micaiah.effiong@gmail.com>
2021-02-28 13:28:43 +0100

    default send to true

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const response = require("./lib/responses");
 
 const init = (config) => {
   return (req, res, next) => {
-    res._simpleSend = config.send || false;
+    // create configuration object if its no passed in
+    if (!config) {
+      config = {};
+    }
+
+    if (config.send === undefined) {
+      config.send = true;
+    }
+    res._simpleSend = config.send;
+
     // add methods to response object
     Object.keys(response).forEach((key) => {
       const value = response[key];
