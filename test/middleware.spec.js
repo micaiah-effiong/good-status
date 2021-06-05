fff51b9d0eccc5a1c5d6c289895e11792d79f2a7
micaiah-effiong <micaiah.effiong@gmail.com>
2021-06-05 18:51:54 +0100

    added test for unofficial/optional status codes

--- test/middleware.spec.js
+++ test/middleware.spec.js
@@ -1,7 +1,7 @@
 const { should } = require("chai");
 const gs = require("../lib");
 
-describe("Initialising gs middleware with wrong congiguration", function () {
+describe("Initialising gs middleware with wrong configuration", function () {
   it("set wrong configuration data type", function () {
     const init = () => gs(" ");
     should().throw(init, "config must be an object");
@@ -36,12 +36,12 @@ describe("Initialise gs middleware", function () {
 
   describe("initialise with statusAttribute", function () {
     const mid = gs({ statusAttribute: "gs" });
-    it("set status code functions on response object", function () {
+    it("set status code functions on res[statusAttribute]", function () {
       const req = {};
       const res = {};
       const next = () => {};
       mid(req, res, next);
-      should().exist(res.gs, "status attribute attached");
+      should().exist(res.gs, "status attribute was not attached");
     });
 
     it("should have ok() method on gs Object", function () {
@@ -49,7 +49,7 @@ describe("Initialise gs middleware", function () {
       const res = {};
       const next = () => {};
       mid(req, res, next);
-      should().exist(res.gs.ok, "ok method exists");
+      should().exist(res.gs.ok, "ok method is not available exists");
     });
 
     it("should have badRequest()", function () {
@@ -57,7 +57,62 @@ describe("Initialise gs middleware", function () {
       const res = {};
       const next = () => {};
       mid(req, res, next);
-      should().exist(res.gs, "badRequest method exists");
+      should().exist(res.gs.badRequest, "badRequest method does not exists");
+    });
+  });
+});
+
+describe("Initialise gs middleware with optional/unofficial status codes", function () {
+  describe("Initialise with  unofficial", function () {
+    it("Unofficial status-codes should be available", function () {
+      const mid = gs({ statusAttribute: "gs", unofficial: true });
+      const req = {};
+      const res = {};
+      const next = () => {};
+      mid(req, res, next);
+      should().exist(
+        res.gs.thisIsFine,
+        "unofficial status-codes  was not attached"
+      );
+    });
+  });
+  describe("Initialise with  infoService", function () {
+    it("Internet Information Services status-codes should be available", function () {
+      const mid = gs({ statusAttribute: "gs", infoService: true });
+      const req = {};
+      const res = {};
+      const next = () => {};
+      mid(req, res, next);
+      should().exist(
+        res.gs.loginTimeout,
+        "infoService status-codes  was not attached"
+      );
+    });
+  });
+  describe("Initialise with  nginx", function () {
+    it("Nginx status-codes should be available", function () {
+      const mid = gs({ statusAttribute: "gs", nginx: true });
+      const req = {};
+      const res = {};
+      const next = () => {};
+      mid(req, res, next);
+      should().exist(
+        res.gs.headerTooLarge,
+        "nginx status-codes  was not attached"
+      );
+    });
+  });
+  describe("Initialise with  cloudflare", function () {
+    it("Cloudflare status-codes should be available", function () {
+      const mid = gs({ statusAttribute: "gs", cloudflare: true });
+      const req = {};
+      const res = {};
+      const next = () => {};
+      mid(req, res, next);
+      should().exist(
+        res.gs.webServerReturnedAnUnknownError,
+        "cloudflare status-codes was not attached"
+      );
     });
   });
 });
